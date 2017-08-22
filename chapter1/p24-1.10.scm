aca4e6d49a1c6cf7cd320c88a700fdfe84b0af98
awefight <547780662@qq.com>
2017-08-23 03:30:16 +0800

    p24-1.10

---
+++ chapter1/p24-1.10.scm
@@ -0,0 +1,18 @@
+
+
+(define (A x y)
+  (cond((= y 0) 0)
+       ((= x 0) (* 2 y))
+       ((= y 1) 2)
+       (else (A (- x 1) 
+		   (A x (- y 1))))))
+
+
+(define (2*n n) (A 0 n))
+
+
+(define (2^n n) (A 1 n))
+
+(define (2^^n  n) (A 2 n))
+
+;if n=4 ,then A 1 A 1 A 1 A 2 (4-3) => A 1 A 1 A 1 2 ,and we know previously A 1 n descripts ;2^n, so A 2 4 equals to 2^2^2^2
