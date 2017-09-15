8d1a881e9b3224e1cbb41c19ea4edc30c7a49b1e
awefight <547780662@qq.com>
2017-09-15 11:38:01 +0800

    p35-1.22

--- chapter1/p35-1.22.scm
+++ chapter1/p35-1.22.scm
@@ -12,24 +12,28 @@
 (define (timed-prime-test n)
   (newline)
   (display n)
-  (start-prime-test n (runtime)))
+  (let ((start-time (real-time-clock)))
+    (start-prime-test n start-time)))
 
 ;if n is prime report elapsed-time
 (define (start-prime-test n start-time)
   (if (prime? n)
-      (report-prime (- (runtime) start-time))))
+      (let ((end-time (real-time-clock)))
+	(report-prime (- end-time start-time)))))
+      
 
 (define (report-prime elapsed-time)
-  (display "used time: ")
-  (display elapsed-time))
+  (display "is prime, used time: ")
+  (display elapsed-time)
+  (display "   multiply sqrt of 10 should be:")
+  (display (* elapsed-time 3.16)))
 
-;==============
+;; ;==============
 (define (prime? x)
   (= (find-smallest-divisor x) x))
 
 (define (find-smallest-divisor n)
-   (test-divisor n 2)    ;;;starting-divisor
-  )
+   (test-divisor n 2))
 
 (define (test-divisor n sd)  ;;; sd = starting divisor
   (cond ((> (square sd) n) n)
@@ -48,51 +52,68 @@
 
 ;; (search-for-prime 101)
 
-
+;;==========================
 (define (search-for-prime n count)
-  (cond ((= count 0)
-	 (display "are primes"))
-	 ((prime? n)
-	  (display n)
-	  (newline)
-	  (search-for-prime (- n 1) (- count 1)))
-	 (else
-	  (search-for-prime (- n 1) count))))
+  (cond((= count 0)
+      (newline)
+      (display "EOF"))
+      (else
+       (cond ((prime? n)
+	   (timed-prime-test n)
+	   (search-for-prime (- n 1) (- count 1)))
+	   (else
+	    (search-for-prime (- n 1) count))))))
 	 
-;; (search-for-prime 101 10)
+	
+
+;;
+(search-for-prime 10000000 3)
+(search-for-prime 100000000 3)
+(search-for-prime 1000000000 3)
+;; (search-for-prime 10000000000 4)
+;; (search-for-prime 100000000000 4)
 
 
-(define (search-for-all-prime n )
-  (define (search-prime n count)
-    (cond ((= count 0)
-	  (display "that is all"))
-	  ((prime? n)
-	   (display n)
-	   (newline)
-	   (search-prime (- n 1) (- count 1)))
-	  (else (search-prime (- n 1) (- count 1)))))
-  (search-prime n (- n 1)))
-;;test
-;;(search-for-all-prime 20)
+;; (define (search-for-all-prime n )
+;;   (define (search-prime n count)
+;;     (cond ((= count 0)
+;; 	  (display "that is all"))
+;; 	  ((prime? n)
+;; 	   (display n)
+;; 	   (newline)
+;; 	   (search-prime (- n 1) (- count 1)))
+;; 	  (else (search-prime (- n 1) (- count 1)))))
+;;   (search-prime n (- n 1)))
+;; ;;test
+;; ;;(search-for-all-prime 20)
 
-(define (search-for-odd-prime n)
-  (define (odd? n)
-    (= (remainder n 2) 1))
-  (define (odd-prime n count)
-    (if (odd? n)	
-	(cond ((= count 1)
-	       (display "that is all"))
-	      ((prime? n)
-	       (display n)
-	       (newline)
-	       (odd-prime (- n 2) (- count 2)))
-	      (else
-	       (odd-prime (- n 2) (- count 2))))
-	(odd-prime (- n 1) (- count 1))))
+;; (define (search-for-odd-prime n)
+;;   (define (odd? n)
+;;     (= (remainder n 2) 1))
+;;   (define (odd-prime n count)
+;;     (if (odd? n)	
+;; 	(cond ((= count 1)
+;; 	       (display "that is all"))
+;; 	      ((prime? n)
+;; 	       (display n)
+;; 	       (newline)
+;; 	       (odd-prime (- n 2) (- count 2)))
+;; 	      (else
+;; 	       (odd-prime (- n 2) (- count 2))))
+;; 	(odd-prime (- n 1) (- count 1))))
   
-  (odd-prime n n))
+;;   (odd-prime n n))
 
-;; test
-;; (search-for-odd-prime 20)
+;; ;; test
+;; ;; (search-for-odd-prime 20)
 
 
+;; ;; (timed-prime-test 1789522)
+;; ;; (timed-prime-test 1999)
+;; ;; (timed-prime-test 19999999999)
+
+;; (define (test-foo)
+;;     (let ((start-time (real-time-clock)))
+;;         (search-for-all-prime 100)
+;;         (- (real-time-clock) start-time)))
+;; (test-foo)
