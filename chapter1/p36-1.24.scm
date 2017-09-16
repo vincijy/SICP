93307e3c27e60f3595f6a5c49c08b3ff7e9261a3
awefight <547780662@qq.com>
2017-09-17 07:09:42 +0800

    finished timing finding prime process using fermat-test

--- chapter1/p36-1.24.scm
+++ chapter1/p36-1.24.scm
@@ -1,18 +1,11 @@
-
-
 (define (square x)
   (* x x))
-
 (define (divide? n d)
   (= (remainder n d) 0))
-
 (define (oven? n)
   (= (remainder n 2) 0))
 ;===============
-
 ; runtime is a primitive in scheme,return a intergal specified the amout of time systerm has been running
-
-
 ;; return running time of testing if a num is prime
 (define (timed-prime-test n)
   (newline)
@@ -25,35 +18,19 @@
       (let ((end-time (real-time-clock)))
 	(report-prime (- end-time start-time)))))
 
-
 (define (report-prime elapsed-time)
   (display "is prime, used time: ")
   (display elapsed-time))
- 
-
-;; ;==============
-;; (define (prime? x)
-;;   (= (find-smallest-divisor x) x))
-
-;; (define (find-smallest-divisor n)
-;;    (test-divisor n 2))
-
-;; (define (test-divisor n sd)  ;;; sd = starting divisor
-;;   (cond ((> (square sd) n) n)
-;; 	 ((divide? n sd)  sd)
-;; 	 (else (test-divisor n (+ sd 1)))))
-
 ;;=======================
 ;;;use fermat-test to find divisor
 ;;if n is prime,choose a num a less than n
 ;;then (reamainder a^n n)=(remaider a n)=a
- 
 (define (prime? n)
+  
   (define (test n times)
     (cond ((= times 0) true)
 	  ((pass-fermat-test? n (random-num n)) (test n (- times 1)))
 	  (else false)))
-    
   (define (pass-fermat-test? n a)
     (= (expmod a n n) a))
   (define (random-num n)
@@ -63,11 +40,7 @@
 	  ((oven? n) (remainder (square (expmod b (/ n 2) m)) m))
 	  (else
 	   (remainder (* b (expmod b (- n 1) m)) m))))
-  (test n 10))
-  
-	
-    
-      
+  (test n 1000))
 ;;==========================
 (define (search-for-prime n count)
   (cond((= count 0)
@@ -79,7 +52,7 @@
 	   (search-for-prime (- n 1) (- count 1)))
 	   (else
 	    (search-for-prime (- n 1) count))))))
-	 
+;===========================
 ;; testing-examples				  
 ;; (search-for-prime 100000 3)
 ;; (search-for-prime 1000000 3)
@@ -103,36 +76,4 @@
 	   (search-prime (- n 1) (- count 1)))
 	  (else (search-prime (- n 1) (- count 1)))))
   (search-prime n (- n 1)))
-;; ;;test
-;; ;;(search-for-all-prime 20)
-
-;; (define (search-for-odd-prime n)
-;;   (define (odd? n)
-;;     (= (remainder n 2) 1))
-;;   (define (odd-prime n count)
-;;     (if (odd? n)	
-;; 	(cond ((= count 1)
-;; 	       (display "that is all"))
-;; 	      ((prime? n)
-;; 	       (display n)
-;; 	       (newline)
-;; 	       (odd-prime (- n 2) (- count 2)))
-;; 	      (else
-;; 	       (odd-prime (- n 2) (- count 2))))
-;; 	(odd-prime (- n 1) (- count 1))))
-  
-;;   (odd-prime n n))
-
-;; ;; test
-;; ;; (search-for-odd-prime 20)
-
-
-;; ;; (timed-prime-test 1789522)
-;; ;; (timed-prime-test 1999)
-;; ;; (timed-prime-test 19999999999)
-
-;; (define (test-foo)
-;;     (let ((start-time (real-time-clock)))
-;;         (search-for-all-prime 100)
-;;         (- (real-time-clock) start-time)))
-;; (test-foo)
+;==========================
