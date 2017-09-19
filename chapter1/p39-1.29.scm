663eb8a83e6174924de476c9e4a75f8a364f8060
awefight <547780662@qq.com>
2017-09-20 00:23:56 +0800

    integral-simpSon

--- chapter1/p39-1.29.scm
+++ chapter1/p39-1.29.scm
@@ -4,23 +4,25 @@
 
 (load "basic-func.scm")
 
-
-(define (sum-term a b next term)
+(define (sum-iterm a b next term)
   (if (> a b)
       0
-      ( + a  (sum-term (next a) b next term))))
+      (+ (term a) (sum-iterm (next a) b next term))))
 
-(define (integral-simpu a b func  n)
+(define (integral-simp a b func  n)
   (define h
     (/ (- b a) n))
   (define (next x)
     (+ x h))
   (define (factor m)
     (cond ((or (= m n) (= m 0)) 1)
- 	  ((oven? m) 2)
-	  (else 4)))
-  (define (term m x)
+	  ((oven? m) 2)
+  	  (else 4)))
+  (define (m x)
+    (
+  (define (term x m)
     (* (factor m) (func x)))
+  
 
   (* (/ h 3) (sum-term a b next term)))
-c
+
