a344f18048d4671b4548a7eb2832a8ceb30d50af
awefight <547780662@qq.com>
2017-09-25 09:58:31 +0800

    define numerator  last term

--- chapter1/p40-1.31.scm
+++ chapter1/p40-1.31.scm
@@ -5,12 +5,44 @@
       1
       (* (term a) (product (next a) b next term))))
 
-(define (factorial a b)
-  (define (identity x)
-    x)
-  (define (inc x)
-    (+ x 1))
-  (product a b inc identity))
+(define (sequence-product a m)
+  (define (next a)
+    (+ a 2))
+  (define (term a)
+    a)
+  (define b
+    (+ a (* 2 (- m 1))))
+  (product a b next term))
+
+(define (denominator a n)
+  (square (sequence-product a (/ n 2))))
+
+(define (numerator a n)
+  (define b
+  (if (oven? n)
+      (/ (square (sequence-product a (+ (/ n 2) 1))) 2 b)
+      (/ (square (sequence-product a (/ (+ n 1) 2))) 2))
+      
+				     
+	 
+
+  
+      
+	
+	  
+    
+  
+	   
+	  
 
 
-(factorial 1 4)
+
+
+
+
+	  
+
+
+
+  
+  
