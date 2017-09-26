2a6c6b4359510954c1f61e6244611b1f927ca4ed
awefight <547780662@qq.com>
2017-09-26 09:34:09 +0800

    caculate pi done

--- chapter1/p40-1.31.scm
+++ chapter1/p40-1.31.scm
@@ -1,32 +1,41 @@
-
+(load "basic-func.scm")
 
 (define (product a b next term)
   (if (> a b)
       1
       (* (term a) (product (next a) b next term))))
 
+
 (define (sequence-product a m)
   (define (next a)
     (+ a 2))
   (define (term a)
     a)
+  ;;last-term
   (define b
     (+ a (* 2 (- m 1))))
   (product a b next term))
 
+;; to deal with 3 3 5 5 7 7
 (define (denominator a n)
   (square (sequence-product a (/ n 2))))
 
+;;to deal with 2 4 4 6 6 8 ..
 (define (numerator a n)
-  (define b
+  (define last-term
+    (if (oven? n)
+	(+ a (* (/ n 2) 2))
+	(+ a (* (/ (- n 1) 2) 2))))
   (if (oven? n)
-      (/ (square (sequence-product a (+ (/ n 2) 1))) 2 b)
-      (/ (square (sequence-product a (/ (+ n 1) 2))) 2))
+      (/ (square (sequence-product a (+ (/ n 2) 1))) 2 last-term)
+      (/ (square (sequence-product a (/ (+ n 1) 2))) 2)))
       
-				     
-	 
 
-  
+(define (pi-helper x y n)
+  (* (/ (numerator x n) (denominator y n)) 4.0))
+
+;; n should be large enough
+(pi-helper 2 3 10000)
       
 	
 	  
