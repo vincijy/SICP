56aff310e7b1b6a759e32b5f23c55e37672b680a
awefight <547780662@qq.com>
2017-09-26 14:14:01 +0800

    iterative accumulate not done yet

--- chapter1/p40-1.32.scm
+++ chapter1/p40-1.32.scm
@@ -1,9 +1,18 @@
 
-(define (accumulate combiner null-value a b term next)
-  (if (>  a b)
-      null-value
-      (combiner (term a) (accumulate combiner null-value (next a) b term next))))
+;; recursive
+;; (define (accumulate combiner null-value a b term next)
+;;   (if (>  a b)
+;;       null-value
+;;       (combiner (term a) (accumulate combiner null-value (next a) b term next))))
 
+
+;; iterative
+(define (accumulate combiner null-value a b term next)
+  (define (iter-helper result)
+    (if (> a b)
+	result
+	(iter-helper (combiner (term (next a)) result))))
+  (iter-helper a))
 
 
 (define (sum a b next term)
@@ -27,7 +36,11 @@
     a)
   (product a b next identity))
 
-(product-identity 1 3)
+
+
+(sum-identity 1 3)
+
+
 
 
 
