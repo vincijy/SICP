66371897aad1ee242be2670a4dca0bb78c94b8aa
awefight <547780662@qq.com>
2017-09-29 07:32:21 +0800

    con-frac iterative

--- chapter1/p47-1.37.scm
+++ chapter1/p47-1.37.scm
@@ -1,11 +1,24 @@
 
 
-(define (cont-frac n d k)
-  (if (= k 1)
-      (/ (n 1) (d 1))
-      (/ (n k) (+ (d k) (cont-frac n d (- k 1))))))
 
-(cont-frac (lambda (x) 1.0) (lambda (x) 1.0) 10)
+;; (define (cont-frac n d k)
+;;   (if (= k 1)
+;;       (/ (n 1) (d 1))
+;;       (/ (n k) (+ (d k) (cont-frac n d (- k 1))))))
+
+;;(cont-frac (lambda (x) 1.0) (lambda (x) 1.0) 15)
+
+
+;; iterative
+(define (cont-frac n d k)
+  (define (iter-helper i result)
+    (if (= i k)
+	result
+	(iter-helper (+ i 1) (/ (n i) (+ (d i) result)))))
+  (iter-helper 1 (/ (n 1) (d 1))))
+
+(cont-frac  (lambda (x) 1.0)  (lambda (x) 1.0) 10)
+
 
 
 
