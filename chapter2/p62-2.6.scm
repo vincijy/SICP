9586ed92c7af8f911a513fd2e220df978636f91d
awefight <547780662@qq.com>
2017-11-13 14:01:02 +0800

    p62-2.6

--- chapter2/p62-2.6.scm
+++ chapter2/p62-2.6.scm
@@ -1,8 +1,34 @@
+ 
 
 
-(define zero (lambda (f) (lambda (x) (x))))
+(define zero
+  (lambda (f)
+    (lambda (x)
+      (x))))
 
 (define (add-1 n)
-  (lambda (f) (lambda (x) (f ((n f) x))))) 
+  (lambda (f)
+    (lambda (x)
+      (f (n f) x))))
 
-(add-1 zero)
+(define one
+  (lambda (f)
+    (lambda (x)
+      (f x))))
+ 
+(define two
+  (lambda (f)
+    (lambda (x)
+      (f (f x)))))
+
+(define +
+  (lambda (m n)
+      (lambda (f)
+	(lambda (x)
+	  (m f (n f x))))))
+
+
+
+
+
+
