f1c910ac386cdfb71928ad6823afc0ecd991e857
awefight <547780662@qq.com>
2017-11-13 22:09:56 +0800

    p66-p68

---
+++ chapter2/p66-p68.scm
@@ -0,0 +1,24 @@
+
+
+(define one-through-four (list 1 2 3 4))
+
+
+(define (list-ref items n)
+  (if (= n 0)
+      (car items)
+      (list-ref (cdr items)  (- n 1))))
+
+
+(define (length items)
+  (if (null? items)
+      0
+      (+ 1 (length (cdr items)))))
+
+
+(define (append list1 list2)
+  (if (null? list1)
+      list2
+      (cons (car list1) (append (cdr list1) list2))))
+      
+
+
