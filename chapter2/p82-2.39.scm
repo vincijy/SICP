e4b8e7190c1edb5c01b4f6bb55992b7d4bce9917
awefight <547780662@qq.com>
2017-11-23 19:34:04 +0800

    reverse by fold-left , fold-right

---
+++ chapter2/p82-2.39.scm
@@ -0,0 +1,34 @@
+
+
+
+
+(define (fold-left op init seqs)
+  (define (iter result rest)
+    (if (null? rest)
+	result
+	(iter (op result (car rest)) (cdr rest))))
+  (iter init seqs))
+
+(define (fold-right op init seqs)
+  (cond ((null? seqs) init)
+	(else (op (car seqs)
+		  (fold-right op init (cdr seqs))))))
+
+
+(define nil '())
+
+(define (reverse-r seqs)
+  (fold-right (lambda(item  rest)
+		(append rest (list item)))
+   nil seqs))
+
+
+(define (reverse-l seqs)
+  (fold-left (lambda(item rest)
+	       (append (list item) rest))
+	     nil seqs))
+
+(reverse (list 1 2 3 4 5 6))
+	     
+	       
+	       
