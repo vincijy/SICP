75dbd5e1350d5c62cc9c4c8b3f0957814ed548fa
awefight <547780662@qq.com>
2017-12-03 01:44:10 +0800

    union set

---
+++ chapter2/p104-2.59.scm
@@ -0,0 +1,24 @@
+
+
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+	((equal? x (car set)) true)
+	(else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (cond((element-of-set? x set) set)
+       (else (cons x set))))
+
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+	((element-of-set? (car set1) set2)
+	 (cons (car set1) (intersection-set (cdr set1)  set2)))
+	(else (intersection-set (cdr set1)  set2))))
+
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+	((null? set2) set1) 
+	((not (element-of-set? (car set1) set2))
+	 (cons (car set1) (union-set (cdr set1) set2)))
+	(else (union-set (cdr set1) set2))))
