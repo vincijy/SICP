ba0b86a715f1491ae58ae90c4b07ddcbf67c5f39
awefight <547780662@qq.com>
2017-12-03 11:37:43 +0800

    adjoin-set

--- chapter2/p105-2.61.scm
+++ chapter2/p105-2.61.scm
@@ -1,10 +1,8 @@
 
 
-(define (union-set set1 set2)
-        (cond ((null? set1) set2)
-	      ((null? set2) set1)
-	      (else (let((x1 (car set1)) (x2 (car set2)))
-		      (cond((= x1 x2) (cons x1 (union-set (cdr set1) (cdr set2))))
-			   ((> x1 x2) (cons x2 (union-set set1 (cdr set2))))
-			   (else
-			    (< x1 x2) (cons x1 (union-set (cdr set1) set2))))))))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+	((= x (car set)) set)
+	((< x (car set)) (cons x set))
+	((> x (car set)) (cons (car set) (adjoin-set x (cdr set))))))
