93f63d05e0379366f57aeddced455d54a71776e4
awefight <awefight@qq.com>
2018-03-14 18:59:20 +0800

    stream map

--- chapter3/p225-3.50.scm
+++ chapter3/p225-3.50.scm
@@ -1,19 +1,23 @@
 
-
+(load "stream.scm")
 ;;;(map + (list 1 2 3) (list 4 5 6)) => (5 7 8)
 
 
-;; (apply stream-map
-;;        (cons proc (map stream-cdr argstream)))
-;; <=> (stream-map proc . (map stream-cdr argstream))
 
 (define (stream-map proc . argstream)
-  (if null? (car argstream))
+  (if (null? (car argstream))
   '()
   (cons-stream
    (apply proc (map stream-car argstream))
    (apply stream-map
-	  (cons proc (map stream-cdr argstream)))))
-	  
+	  (cons proc (map stream-cdr argstream))))))
 
+
+;; test
+(define add (lambda(x y) (+ x y)))
+
+(define s1 (stream-enumerate-interval 1 10))
+(define s2 (stream-enumerate-interval 11 20))
+
+(display-stream (stream-map add s1 s2))
 
