3962623a5ea95daa033416044d5ae5fe360f3817
awefight <awefight@qq.com>
2018-03-14 15:21:18 +0800

    p226-3.51.scm

---
+++ chapter3/p226-3.51.scm
@@ -0,0 +1,27 @@
+
+(load "stream.scm")
+
+(define (display-line x)
+  (newline)
+  (display x))
+
+
+(define (show x)
+  (display-line x)
+  x)
+
+(define x (stream-map show (stream-enumerate-interval 0 10)))
+
+;; (stream-ref x 5)
+
+(stream-ref x 7)
+
+
+
+
+
+  
+
+  
+  
+
