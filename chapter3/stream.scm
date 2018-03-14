6fa1b909ff3c83bb14e98ec919f33f0a20de4497
awefight <awefight@qq.com>
2018-03-14 16:51:21 +0800

    some stream function

---
+++ chapter3/stream.scm
@@ -0,0 +1,19 @@
+
+
+(define (stream-enumerate-interval low high)
+  (if(> low high)
+     the-empty-stream
+     (cons-stream low
+		  (stream-enumerate-interval (+ low 1) high))))
+
+
+(define (display-line x)
+  (newline)
+  (display x))
+
+(define (display-stream stream)
+  (stream-for-each display-line stream))
+
+
+
+(display-stream (stream-enumerate-interval 1 10))
